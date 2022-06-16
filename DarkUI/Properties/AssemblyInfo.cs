79c1c97a965b95eaac4b9ff06bc44baae6dd13a7
Shelley <shelley@firestationsoftware.com>
2022-06-16 06:52:04 -0400

    reworked project; set up nuget

--- DarkUI/Properties/AssemblyInfo.cs
+++ DarkUI/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DarkUI")]
-[assembly: AssemblyDescription("Dark themed control and docking library for .NET WinForms.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Robin Perris")]
-[assembly: AssemblyProduct("DarkUI")]
-[assembly: AssemblyCopyright("Copyright © Robin Perris")]
-[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyTitle("DarkUI-Starlit")]
+//[assembly: AssemblyDescription("Dark themed control and docking library for .NET WinForms.")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Starlight Consultants, LLC")]
+//[assembly: AssemblyProduct("DarkUI-Starlit")]
+//[assembly: AssemblyCopyright("Copyright © 2022 Robin Perris and Starlight Consultants, LLC")]
+[assembly: AssemblyTrademark("Used under MIT license. Original by Robin Perris")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f19472f5-8c44-4c51-a8a0-b9de5f555255")]
+[assembly: Guid("07348789-8BD5-4433-B730-866DB9DCC16C")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+//[assembly: AssemblyVersion("2.2.0.0")]
+//[assembly: AssemblyFileVersion("2.2.0.0")]
